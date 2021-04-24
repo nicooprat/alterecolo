29c1a95fb02df6911c10a1e51463df92fec1c2ec
Nico Prat <nicooprat@gmail.com>
2021-04-24 10:12:07 +0200

    Update lint rules
    from https://gist.github.com/lewebsimple/a086879024556b5f86783c75e41956f1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,35 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
   ],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'vue/attribute-hyphenation': ['error', 'never'],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+  ignorePatterns: ['dist'],
 }
